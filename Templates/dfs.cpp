3a803cf3b849d96146ac157bc948d6eb8675a4e9
Gurnoor Sohal <gnrsohal@gmail.com>
2025-07-04 23:16:10 +0530

    dfs with time

--- Templates/dfs.cpp
+++ Templates/dfs.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(int u, const vector<vector<int>>& adj, vector<bool>& vis) {
-    vis[u] = true;
-    for (int v : adj[u]) {
-        if (!vis[v]) dfs(v, adj, vis);
-    }
+vector<vector<int>> adj;
+int n;
+
+vector<int> color;
+
+vector<int> time_in, time_out;
+int dfs_timer = 0;
+
+void dfs(int v) {
+    time_in[v] = dfs_timer++;
+    color[v] = 1;
+    for (int u : adj[v])
+        if (color[u] == 0)
+            dfs(u);
+    color[v] = 2;
+    time_out[v] = dfs_timer++;
 }
