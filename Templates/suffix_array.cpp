1b25af6cdc3765bd6013167ca8c7ecdd9cd25aaa
Gurnoor Sohal <gnrsohal@gmail.com>
2025-07-08 23:42:14 +0530

    comparing substr using lcp
    for comparing substr in o(1) using suffix arrays

--- Templates/suffix_array.cpp
+++ Templates/suffix_array.cpp
@@ -45,3 +45,10 @@ vector<int> suffix_array_construction(string s) {
     sorted_shifts.erase(sorted_shifts.begin());
     return sorted_shifts;
 }
+// to comapre two substrings of length l from string s starting at indices i and j
+// k = floor(log2(l))
+int compare(int i, int j, int l, int k) {
+    pair<int, int> a = {c[k][i], c[k][(i+l-(1 << k))%n]};
+    pair<int, int> b = {c[k][j], c[k][(j+l-(1 << k))%n]};
+    return a == b ? 0 : a < b ? -1 : 1;
+}
