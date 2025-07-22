ed60433de15fcaa724c47e9422bbe1d905dac5d6
Gurnoor Sohal <gnrsohal@gmail.com>
2025-07-22 12:47:44 +0530

    suffix array qs week7

---
+++ Submissions/uva_dna.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <string>
+#define ll long long
+using namespace std;
+// template copied from cp repo
+vector<int> sort_cyclic_shifts(string const& s) {
+    int n = s.size();
+    const int alphabet = 256;
+    vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
+    for (int i = 0; i < n; i++) cnt[s[i]]++;
+    for (int i = 1; i < alphabet; i++) cnt[i] += cnt[i-1]; //prefix sum
+    for (int i = 0; i < n; i++) p[--cnt[s[i]]] = i; //permutation array for 1-char substrings
+    c[p[0]] = 0; //c is the equivalence class array
+    int classes = 1; //num of eq classes
+    for (int i = 1; i < n; i++) {
+        if (s[p[i]] != s[p[i-1]]) classes++;
+        c[p[i]] = classes - 1;
+    }
+    vector<int> pn(n), cn(n);
+    //prefix doubling
+    for (int h = 0; (1 << h) < n; ++h) {
+        for (int i = 0; i < n; i++) {
+            pn[i] = p[i] - (1 << h);
+            if (pn[i] < 0) pn[i] += n;
+        }
+        fill(cnt.begin(), cnt.begin() + classes, 0);
+        for (int i = 0; i < n; i++)
+            cnt[c[pn[i]]]++;
+        for (int i = 1; i < classes; i++)
+            cnt[i] += cnt[i-1];
+        for (int i = n-1; i >= 0; i--)
+            p[--cnt[c[pn[i]]]] = pn[i];
+        cn[p[0]] = 0;
+        classes = 1;
+        for (int i = 1; i < n; i++) {
+            pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
+            pair<int, int> prev = {c[p[i-1]], c[(p[i-1] + (1 << h)) % n]};
+            if (cur != prev)
+                ++classes;
+            cn[p[i]] = classes - 1;
+        }
+        c.swap(cn);
+    }
+    return p;
+}
+
+vector<int> lcp_construction(string const& s, vector<int> const& p) {
+    int n = s.size();
+    vector<int> rank(n, 0);
+    for (int i = 0; i < n; i++)
+        rank[p[i]] = i;
+
+    int k = 0;
+    vector<int> lcp(n-1, 0);
+    for (int i = 0; i < n; i++) {
+        if (rank[i] == n - 1) {
+            k = 0;
+            continue;
+        }
+        int j = p[rank[i] + 1];
+        while (i + k < n && j + k < n && s[i+k] == s[j+k])
+            k++;
+        lcp[rank[i]] = k;
+        if (k)
+            k--;
+    }
+    return lcp;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t; cin>>t;
+    while(t--) {
+        string s1, s2; cin>>s1>>s2;
+        int n = s1.length();
+        string s = s1 + '$' + s2;
+        vector<int> sa = sort_cyclic_shifts(s);
+        vector<int> lcp = lcp_construction(s, sa);
+        int max_len = 0;
+        for(int i=1; i<s.length(); i++) {
+            bool b1 = sa[i - 1] < n ;
+            bool b2 = sa[i] < n;
+            if(b1 != b2) max_len = max(max_len, lcp[i]);
+        }
+        if(max_len == 0) {
+            cout<<"No common dna seg"<<endl;
+            continue;
+        }
+        set<string> ans;
+        for(int i=1; i<s.length(); i++) {
+            bool b1 = sa[i - 1] < n ;
+            bool b2 = sa[i] < n;
+            if(b1 != b2 && lcp[i-1]==max_len) ans.insert(s.substr(sa[i], max_len));
+        }
+
+    }
+}
